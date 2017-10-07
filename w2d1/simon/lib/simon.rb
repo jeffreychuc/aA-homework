b3b4a97da8a4434783cbb8ae7ff1d9ce9c02e940
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-10-07 16:38:16 -0700

    Adds show_sequence, require_sequence, round_success_message, game_over_message, and reset_game

--- w2d1/simon/lib/simon.rb
+++ w2d1/simon/lib/simon.rb
@@ -28,14 +28,24 @@ class Simon
 
   def show_sequence
     add_random_color
-    # commented out below lines for rspec testing
-    # puts @seq.to_s
-    # sleep(1)
-    # system("clear")
+    seq_s = ""
+    @seq.each do |color|
+      seq_s += color + " "
+    end
+    puts "The sequence is:"
+    puts seq_s
+    sleep(3)
+    system("clear")
   end
 
   def require_sequence
-
+    @sequence_length.times do |i|
+      print "What was color \# #{i+1}: "
+      guess = gets.chomp
+      if guess != @seq[i]
+        @game_over = true
+      end
+    end
   end
 
   def add_random_color
@@ -43,15 +53,16 @@ class Simon
   end
 
   def round_success_message
-
+    puts "You guessed the sequence correctly!"
+    system("clear")
   end
 
   def game_over_message
-
+    puts "Sorry you lost!"
   end
 
   def reset_game
-
+    initialize
   end
 end
 
