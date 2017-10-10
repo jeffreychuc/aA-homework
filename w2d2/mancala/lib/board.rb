3a5caabfd6d96561106343e5f0184077a42c521c
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-10-09 22:34:21 -0700

    Adds finished Board to mancala

--- w2d2/mancala/lib/board.rb
+++ w2d2/mancala/lib/board.rb
@@ -3,6 +3,8 @@ class Board
   SIZE = 14
   def initialize(name1, name2)
     @cups = make_cups
+    @name1 = name1
+    @name2 = name2
     @names = {name1 => 13, name2 => 6}
   end
 
@@ -29,16 +31,13 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
-    # if valid_move?(start_pos)
-      hand = @cups[start_pos]
-      @cups[start_pos] = Array.new
-    # end
-    curr_pos = start_pos + 1
+    hand = @cups[start_pos]
+    @cups[start_pos] = Array.new
+    curr_pos = start_pos
     until hand.empty?
-      # p [curr_pos, @names[current_player_name], current_player_name]
-      @cups[curr_pos].push(hand.pop) unless curr_pos == @names[current_player_name]
       curr_pos += 1
-      curr_pos %= 13
+      curr_pos %= 14
+      @cups[curr_pos].push(hand.pop) unless curr_pos == @names[current_player_name]
     end
     render
     next_turn(curr_pos)
@@ -46,6 +45,13 @@ class Board
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -61,8 +67,16 @@ class Board
   end
 
   def winner
-    if one_side_empty?
-
+    p1 = @cups[6].count
+    p2 = @cups[13].count
+    if p1 == p2
+      return :draw
+    end
+    if p1 > p2
+      return @name1
+    else
+      return @name2
+    end
   end
 end
 
