97a47e7e2bbc66fe400df173419baeca214e5b4a
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-10-22 11:27:41 -0700

    Add index render, create, and destroy controller action.

--- w4d1/library_controller/app/controllers/books_controller.rb
+++ w4d1/library_controller/app/controllers/books_controller.rb
@@ -1,18 +1,22 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
   end
 
   def new
     # your code here
+    render :new
   end
 
   def create
     # your code here
+    Book.create!(book_params)
+    redirect_to books_url
   end
 
   def destroy
-    # your code here
+    Book.find(params[:id]).delete
+    redirect_to books_url
   end
 
   private
