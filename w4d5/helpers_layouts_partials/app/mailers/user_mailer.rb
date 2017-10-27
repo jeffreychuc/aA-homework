38f3303794e7d8eb642044df61e73f763d2a42ee
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-10-26 22:32:36 -0700

    Add action_mailer welcome message

--- w4d5/helpers_layouts_partials/app/mailers/user_mailer.rb
+++ w4d5/helpers_layouts_partials/app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'jchuc@appacademy.io'
+
+  def welcome_email(user)
+    # so i can access username later
+    @user = user
+    mail(to: @user.username, subject: 'Welcome to 99 cats!')
+  end
 end
