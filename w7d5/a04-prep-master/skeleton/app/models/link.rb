a2a6311d5d889533b981ed323e51e7793ced6570
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-11-16 21:14:16 -0800

    add user auth practice

---
+++ w7d5/a04-prep-master/skeleton/app/models/link.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  url        :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Link < ApplicationRecord
+  validates :title, :url, presence: true
+
+  has_many :comments
+  belongs_to :user
+end
---
+++ w7d5/a04-prep-master/skeleton/app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :password, length: {minimum: 6, allow_nil: true}
+  validates :username, presence: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true
+
+  before_validation :ensure_session_token
+  
+  has_many :links
+  has_many :comments
+
+  attr_reader :password
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if !user.nil? and user.is_password?(password)
+  end
+end
